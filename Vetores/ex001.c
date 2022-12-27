d5ecd5dfb706bd448875d33e28da8d50eea8010a
Ana Luiza Pfeilsticker de Oliveira Araújo Tristão <110688069+ana-pfeilsticker@users.noreply.github.com>
2022-12-27 08:45:26 -0300

    Add files via upload

---
+++ Vetores/ex001.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main(){
+    int menor, smenor,tmenor;
+    int i;
+    int c = 0;
+    
+    scanf("%d", &i);
+    
+    int num[i];
+    
+    while (c<i){
+        scanf("%d", &num[c]);
+        if (c==0){
+            menor = num[c];
+        }
+        if (num[c]< menor){
+            menor = num[c];
+        }
+        c++;
+    }
+    c = 0;
+    while(c<i){
+        if ((c ==0)&&(num[c]!= menor)){
+            smenor = num[c];
+        }
+        if ((num[c]!= menor)&&(num[c] < smenor)){
+            smenor = num[c];
+        }
+        c++;
+    }
+    c = 0;
+    while(c<i){
+        if ((c ==0)&&(num[c]!= smenor)&&(num[c]!= smenor)){
+            tmenor = num[c];
+        }
+        if ((num[c]!= menor)&&(num[c] < tmenor)&&(num[c]!= smenor)){
+            tmenor = num[c];
+        }
+        c++;
+    }
+    printf("%d\n", smenor);
+    printf("%d\n", tmenor);
+    return 0;
+}
